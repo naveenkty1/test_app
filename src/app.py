19c0aa1331c384caf2940695c11e230f687473a8
Naveen <naveenkty1@gmail.com>
2018-11-11 05:19:21 +0000

    chnges

--- src/app.py
+++ src/app.py
@@ -1,12 +1,28 @@
 # sample_web/app.py
 
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def test_app:
-    return 'Testing dbdvb  Python DevOps Platform!'
+@app.route('/test1')
+def test_app():
+    return 'Testing Python DevOps Platform!'
+
+@app.route('/test2')
+def test_app():
+    return 'Testing Angular DevOps Platform!'
+
+@app.route('/test3')
+def test_app():
+    return 'Testing Scala DevOps Platform!'
+
+@app.route('/test4')
+def test_app():
+    return 'Testing XML DevOps Platform!'
+
+@app.route('/test5')
+def test_app():
+    return 'Testing JAVA DevOps Platform!'
 
 
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
